91496434d8c6d9f35578a3c3768d3a770c2db787
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 20:10:46 +0300

    Add project files.

---
+++ Northwind_Select/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind_Select.Models;
+using System.Diagnostics;
+
+namespace Northwind_Select.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindContext context = new NorthwindContext();
+        public IActionResult Index()
+        {
+
+            List<Product> products = context.Products.ToList();
+             return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public IActionResult DiscountPage()
+        {
+            return View();
+        }
+        public IActionResult DetayliSelect()
+        {
+
+            List<Product> products = context.Products.ToList();
+            return View(products);
+        } 
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Northwind_Select/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind_Select.Models
+{
+    public class Product
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+         
+    }
+}
+
